80073ab766f4bac689cd17ae2887fbbd7a7119b4
Jamie Arkin <jamielaurenarkin@gmail.com>
2017-11-15 17:24:05 -0500

    small change for mobile

--- src/components/Register/RegisterModal.js
+++ src/components/Register/RegisterModal.js
@@ -8,7 +8,7 @@ class RegisterModal extends Component {
         <div className="modal" id="registerModal" tabIndex="-1" role="dialog" aria-labelledby="registerModal" aria-hidden="true">
           <div className="modal-dialog" role="document">
             <div className="modal-content">
-              <div className="modal-header sticky-top d-flex align-items-center">
+              <div className="modal-header sticky-top d-sm-flex align-items-sm-center">
                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">x</button>
               </div>
               <div className="modal-body d-sm-flex align-items-sm-center">
