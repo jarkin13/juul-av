29da7441cab58589e0ac50d45e53c19292a718c1
Jamie Arkin <jamielaurenarkin@gmail.com>
2017-11-15 02:33:24 -0500

    test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import $ from 'jquery';
+import Tether from 'tether';
+import Popper from 'popper.js';
+
+window.jQuery = $;
+window.$ = $;
+window.Tether = Tether;
+window.Popper = Popper;
+
+const bootstrap = require('bootstrap');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
